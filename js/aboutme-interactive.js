24c707a17005b24c4a2a2c5e134087548f0e58aa
Osvaldotr07 <osvaldotr07@outlook.com>
2019-04-08 00:14:39 -0500

    Se hace responsivo la hoja aboutme

--- js/aboutme-interactive.js
+++ js/aboutme-interactive.js
@@ -1,4 +1,14 @@
 const $contenido = document.getElementById('contenido');
+const $menuButton = document.getElementById('menu-button')
+const $inicioNavegacion = document.getElementById('menu-navegacion')
+
+$menuButton.addEventListener('click', () => {
+	$inicioNavegacion.classList.toggle('active')
+})
+
+$inicioNavegacion.addEventListener('click', (evt) => {
+	$inicioNavegacion.classList.remove('active')
+})
 
 window.addEventListener('load', (evt) => {
 	if($contenido.classList.contains('hide')){
